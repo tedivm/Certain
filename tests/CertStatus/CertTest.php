697b3d8cdb85cedeb1665e3b1d193d6720d86a47
Robert Hafner <tedivm@tedivm.com>
2014-01-29 01:14:40 -0800

    Added first test class in and made a small change to the test suite's bootstrap file

---
+++ tests/CertStatus/CertTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the CertStatus package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CertStatus\Test;
+
+use CertStatus\Cert;
+
+class CertTest extends PHPUnit_Framework_TestCase {
+
+    static public function getTestCert($name)
+    {
+        $path = TESTING_DIRECTORY . '/Certs/' . $name . '/';
+        if(!file_exists(($path) || !is_dir($path))
+            throw new \Exception('Requested certificate files not found at: ' . $path);
+
+        $certPaths = array();
+
+        $files = scandir($path);
+        foreach($files as $file)
+        {
+            if($file == '.' || $file == '..')
+                continue;
+
+            $certPaths[] = $file;
+        }
+
+        return Cert::getCertFromFiles($certPaths);
+    }
+
+    public function testGetCertFromFiles()
+    {
+
+    }
+
+    public function testGetCertFromServer()
+    {
+
+    }
+
+    public function testSetFromChain()
+    {
+
+    }
+
+    public function testSetHost()
+    {
+
+    }
+
+    public function getParent()
+    {
+
+    }
+
+}
+ 
