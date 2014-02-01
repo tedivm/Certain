3789aa14cd627790d93de9ead5620ca168cd61d9
Robert Hafner <tedivm@tedivm.com>
2014-02-01 01:51:08 -0800

    Added tests for Cert::getParameters

--- tests/Certain/Test/CertTest.php
+++ tests/Certain/Test/CertTest.php
@@ -40,6 +40,16 @@ class CertTest extends \PHPUnit_Framework_TestCase
 
     }
 
+    public function testGetParameters()
+    {
+        $cert = static::getTestChain('Google');
+
+        $parameters = $cert->getParameters();
+        $this->assertInternalType('array', $parameters, 'getParameters returns array');
+        $this->assertGreaterThan(0, count($parameters), 'Parameters array not empty.');
+    }
+
+
     public function testGetParent()
     {
         $cert = $this->getTestChain('Google');
