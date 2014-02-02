347948beda9ba41749462b334208ba9df5b96962
Robert Hafner <tedivm@tedivm.com>
2014-02-02 02:33:44 -0800

    Added proper exceptions into the CertFactory class

--- tests/Certain/Test/CertFactoryTest.php
+++ tests/Certain/Test/CertFactoryTest.php
@@ -13,6 +13,7 @@ namespace Certain\Test;
 
 use Certain\CertFactory;
 
+
 class CertFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetCertFromChain()
@@ -42,9 +43,45 @@ class CertFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\Certain\Cert', $grandParent, 'getCertFromFiles properly populates grand parent');
     }
 
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testGetCertFromFiles_PermissionException()
+    {
+        $files = array(
+            '/root/cert1.crt',
+            '/root/cert2.crt',
+            '/root/cert3.crt',
+        );
+        CertFactory::getCertFromFiles($files);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testGetCertFromFiles_WrongPathException()
+    {
+        $files = array(
+            '/fakepath/cert1.crt',
+            '/fakepath/cert2.crt',
+            '/fakepath/cert3.crt',
+        );
+        CertFactory::getCertFromFiles($files);
+    }
+
+
     public function testGetCertFromServer()
     {
         $cert = CertFactory::getCertFromServer('www.google.com', 443);
         $this->assertInstanceOf('\Certain\Cert', $cert, 'getCertFromFiles returns Cert.');
     }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testGetCertFromServerException()
+    {
+        CertFactory::getCertFromServer('localhost', 30000);
+    }
+
 }
